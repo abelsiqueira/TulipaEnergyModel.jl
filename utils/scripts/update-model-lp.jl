2873ccdd4dda76b2224806b582844a9aeaa4f72f
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2025-03-04 10:29:24 +0100

    Create script to create all lp files of test/input cases

---
+++ utils/scripts/update-model-lp.jl
@@ -0,0 +1,27 @@
+# Run from project folder with
+#
+#   julia --project=. utils/scripts/update-model-lp.jl
+#
+using TulipaEnergyModel, TulipaIO, DuckDB
+
+model_lp_folder = joinpath("benchmark", "model-lp-folder")
+if isdir(model_lp_folder)
+    rm(model_lp_folder; force = true, recursive = true)
+end
+mkdir(model_lp_folder)
+
+for folder in readdir("test/inputs"; join = true)
+    isdir(folder) || continue
+
+    @info "Running run_scenario for $folder"
+
+    con = DBInterface.connect(DuckDB.DB)
+    schemas = TulipaEnergyModel.schema_per_table_name
+    TulipaIO.read_csv_folder(con, folder; schemas)
+    TulipaEnergyModel.run_scenario(con; write_lp_file = true)
+
+    lp_filename = basename(folder) * ".lp"
+    @info "Storing model.lp into $model_lp_folder/$lp_filename"
+
+    mv("model.lp", joinpath(model_lp_folder, lp_filename))
+end
